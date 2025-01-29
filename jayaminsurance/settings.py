381c30b49da040ba207970e03ee73bc2671b0fe0
Aravinthishu <aravintharavinth7746@gmail.com>
2025-01-29 15:24:31 +0530

    update database3

--- jayaminsurance/settings.py
+++ jayaminsurance/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'jayaminsurance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # MySQL database engine
+        'NAME': 'jayam',          # Your MySQL database name
+        'USER': 'Admin',             # Your MySQL username
+        'PASSWORD': 'Webteam@Ec4you',     # Your MySQL password
+        'HOST': 'localhost',                   # Database host, use 'localhost' or an IP address
+        'PORT': '3306',                        # Default MySQL port
     }
 }
 
