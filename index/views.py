259987350c1a215d643621d7f56de391b1234987
Aravinthishu <aravintharavinth7746@gmail.com>
2025-01-29 15:02:14 +0530

    update code 29/01/2025

--- index/views.py
+++ index/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from .models import *
-# Create your views here.
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.core.mail import EmailMessage
+from .models import Contact, HeroSection, HeroImage, AboutSection, InsuranceSection, Testimonial, PartnerLogo, Certificate
 
 def index(request):
     heroslider = HeroSection.objects.first()
@@ -12,14 +14,55 @@ def index(request):
     logos = PartnerLogo.objects.all()
     certificates = Certificate.objects.all()
     
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        email = request.POST.get("email", None)
+        phone = request.POST.get("phone")
+        insurance_type = request.POST.get("insurance_type")
+
+        if not insurance_type:
+            messages.error(request, "Please select an insurance type.")
+            return redirect("index:index")
+
+        # Save the contact information
+        Contact.objects.create(name=name, email=email, phone=phone, insurance=insurance_type)
+
+        # Render email content using an HTML template
+        subject = f"New Insurance Request: {insurance_type.capitalize()} Insurance"
+        context = {
+            'name': name,
+            'email': email,
+            'phone': phone,
+            'insurance_type': insurance_type,
+        }
+        email_body = render_to_string('contact_email.html', context)
+
+        if insurance_type == 'life' or insurance_type == 'health':
+            to_email = [settings.ADMIN_EMAIL2, settings.ADMIN_EMAIL]  # Replace with your email address
+        else:
+            to_email = [settings.ADMIN_EMAIL]
+            
+        from_email = settings.ADMIN_EMAIL  # Replace with a valid from address
+        
+        try:
+            email_message = EmailMessage(subject, email_body, from_email, to_email)
+            email_message.content_subtype = 'html'  # Set the email content to HTML
+            email_message.send()
+            print("Email sent successfully!")
+            messages.success(request, "Your message has been submitted successfully!")
+        except Exception as e:
+            print("An error occurred while sending the email:", e)
+            messages.error(request, f"An error occurred while sending the email: {e}")
+
+        return redirect("index:index")
+    
     context = {
         'heroslider': heroslider,
         'about': about,
         'insurance': insurance,
-        'testimonials':testimonials,
-        'logos':logos,
+        'testimonials': testimonials,
+        'logos': logos,
         'certificates': certificates,
         'additional_images': additional_images,
-        
     }
     return render(request, 'index.html', context)
--- jayaminsurance/settings.py
+++ jayaminsurance/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(4@93k!*d#^iu&728f6=02uo!kr^mvs0+4vh=nm%9o5z$@a!20'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -134,6 +136,19 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  # Path to store media files
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Email configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER=config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER_NAME=config('EMAIL_HOST_USER_NAME')
+ADMIN_EMAIL=config('ADMIN_EMAIL')
+ADMIN_EMAIL2=config('ADMIN_EMAIL2')
+
+#Jazzmin Settings
 
 JAZZMIN_SETTINGS = {
     "site_title": "Jayam Insurance",  # Title of the admin site
